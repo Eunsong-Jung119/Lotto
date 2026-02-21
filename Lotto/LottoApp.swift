be55d085129d821be2de11d039a3bcde62f898a2
Eunsong-Jung119 <eunsongjung1997@gmail.com>
2026-02-21 14:58:17 +0900

    Initial Commit

---
+++ Lotto/LottoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottoApp.swift
+//  Lotto
+//
+//  Created by Eunsong Jung on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct LottoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LottoTests/LottoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LottoTests.swift
+//  LottoTests
+//
+//  Created by Eunsong Jung on 2/21/26.
+//
+
+import Testing
+@testable import Lotto
+
+struct LottoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
